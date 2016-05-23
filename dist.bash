5ef92ca6ec639304c841ef6be57746bfb3342a47
Benoît Zugmeyer <bzugmeyer@gmail.com>
2016-05-23 11:17:08 +0200

    Generate unminified dist bundles

--- dist.bash
+++ dist.bash
@@ -9,9 +9,11 @@ PATH="$(npm bin):$PATH"
 build () {
     local output=$1
     shift
-    echo "Building dist/$output..."
-    browserify -s dateTimezone -g uglifyify -e index.js "${@}" -o dist/$output
+    echo "Building dist/$output.js..."
+    browserify -s dateTimezone -e index.js "${@}" -o dist/$output.js
+    echo "Building dist/$output.min.js..."
+    browserify -s dateTimezone -g uglifyify -e index.js "${@}" -o dist/$output.min.js
 }
 
-build date-timezone.js -x moment-timezone
-build date-timezone-with-moment.js
+build date-timezone -x moment-timezone
+build date-timezone-with-moment
